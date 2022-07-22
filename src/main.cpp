461098be50d829d76a544f92e1e8119717da7f09
Dwyriel <dwyriel333@hotmail.com>
2022-07-22 17:18:20 -0300

    Output to file
    main() needs major reword

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,9 @@ static std::string NO_TEXT_ARG_PASSED = "No text passed";
 static std::string NO_FILE_ARG_PASSED = "No file passed";
 static std::string TEXT_OUTPUT_MESSAGE = "UwUfied text:";
 static std::string FILE_OUTPUT_MESSAGE = "UwUfied file:";
-static std::string FILE_DOESNT_EXIST = "File doesn't exist";
+static std::string OUTPUTING_TO_FILE = "Outputing to file..";
+static std::string FILE_DOESNT_EXIST = "Input file doesn't exist";
+static std::string FILE_ALREADY_EXIST = "Output file already exist, pass --force to overwrite it";
 #endif
 #if UWUIFIED == 1
 static std::string NO_ARG_PASSED = "Nyo awguwumewnts passed, twy -h ow --hewp fow hewp";
@@ -28,12 +30,14 @@ static std::string NO_TEXT_ARG_PASSED = "Nyo text passed";
 static std::string NO_FILE_ARG_PASSED = "Nyo fiwe passed";
 static std::string TEXT_OUTPUT_MESSAGE = "UwUfied text:";
 static std::string FILE_OUTPUT_MESSAGE = "UwUfied fiwe:";
-static std::string FILE_DOESNT_EXIST = "Fiwe doesn't exist";
+static std::string OUTPUTING_TO_FILE = "Outputing to fiwe..";
+static std::string FILE_DOESNT_EXIST = "Input fiwe doesn't exist";
+static std::string FILE_ALREADY_EXIST = "Output fiwe aw-weady exist, pass --fowce to ovew-wwite it";
 #endif
 
 // Arguments
 const std::string help1 = "-h", help2 = "--help", help3 = "--hewp";
-const std::string file_cmd = "-f", text_cmd = "-t", output_file_cmd = "-o";
+const std::string file_cmd = "-f", text_cmd = "-t", output_file_cmd = "-o", force_file_write1 = "--force", force_file_write2 = "--fowce";
 
 static std::string EXEC_FOLDER_PATH = "";
 static std::string EXECUTABLE_PATH = "";
@@ -49,8 +53,8 @@ void setProgramPaths(char* relative_path){
 }
 
 int main(int argc, char **argv) {//TODO rework all this garbage
-    std::string firstCommand, text, secondCommand, outputFile;
-    bool outputToFile = false;
+    std::string firstCommand, text, secondCommand, outputFile, thirdCommand;
+    bool outputToFile = false, shouldForceFileWrite = false;
     setProgramPaths(argv[0]);
     if(argc==1){
         Logger::LogWarning(NO_ARG_PASSED);
@@ -79,10 +83,14 @@ int main(int argc, char **argv) {//TODO rework all this garbage
             secondCommand = argv[3];
             outputFile = argv[4];
             if(!secondCommand.compare(output_file_cmd)){
-                //TODO verify if output file is valid (if it doesn't exist already, if it can be created, etc)
-                Logger::LogInfo("Outputing to file..");
+                Logger::LogInfo(OUTPUTING_TO_FILE);
                 outputToFile = true;
             }
+        }
+        if(argc>5){
+            thirdCommand = argv[5];
+            if(!thirdCommand.compare(force_file_write1) || !thirdCommand.compare(force_file_write2))
+                shouldForceFileWrite = true;
         }
         if(!firstCommand.compare(text_cmd)){
             UwUifier::uwuify(text);
@@ -101,7 +109,7 @@ int main(int argc, char **argv) {//TODO rework all this garbage
                 Logger::LogError(FILE_DOESNT_EXIST);
                 return 1;
             }
-            file.open(filePath);
+            file.open(filePath, std::ios::in);
             while (std::getline(file, line)){
                 stream << line << std::endl;
                 text += stream.str();
@@ -116,7 +124,17 @@ int main(int argc, char **argv) {//TODO rework all this garbage
             }
         }
     }
-    //TODO writeToFile
+    if(!shouldForceFileWrite){
+        if(std::filesystem::exists(outputFile)){
+            Logger::LogError(FILE_ALREADY_EXIST);
+            return 1;
+        }
+    }
+    std::fstream file;
+    file.open(outputFile, std::ios::out | std::ios::trunc);
+    file << text;
+    file.close();
+    Logger::LogInfo("Done!");
     return 0;
 }
 
