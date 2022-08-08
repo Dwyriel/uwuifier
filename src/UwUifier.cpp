abdcd8673b8ccac09ccc8d7a8afc1fdd162da5c7
Dwyriel <dwyriel333@hotmail.com>
2022-08-08 12:37:21 -0300

    UwU & WIP stuttering

--- src/UwUifier.cpp
+++ src/UwUifier.cpp
@@ -3,6 +3,16 @@
 
 namespace UwUifier{
 
+    static std::string trim(std::string &textToTrim){
+        const std::string whitespace = " \t";
+        size_t firstCharacterPosition = textToTrim.find_first_not_of(whitespace);
+        if(firstCharacterPosition == std::string::npos)
+            return "";
+        size_t lastCharacterPosition = textToTrim.find_last_not_of(whitespace);
+        size_t lastCharacterPositionAfterTrim = lastCharacterPosition - firstCharacterPosition;
+        return textToTrim.substr(firstCharacterPosition, lastCharacterPositionAfterTrim + 1);
+    }
+
     static std::vector<std::string> swapPairs {
         "Na", "Nya",
         "na", "nya",
@@ -15,6 +25,8 @@ namespace UwUifier{
         "l", "w",
         "L", "W"
     };
+
+    static std::vector<std::string> stutterLetters {" p", " P", " b", " B" , "\np", "\nP", "\nb", "\nB"};
 
     static void swapWith(std::string &text, std::string &textToFind, std::string &textToImplement){
         size_t pos = text.find(textToFind);
@@ -48,7 +60,55 @@ namespace UwUifier{
         }
     }
 
-    static void uwuifyByWord(std::string &text){
+    static void addStutterByWord(std::vector<std::string> &words, const float &stutterFrequency){
+        float counter = 0;
+        for(std::string &word : words){
+            for(std::vector<std::string>::iterator i = stutterLetters.begin(); i < stutterLetters.end(); i++) {
+                size_t pos = word.find(*i);
+                if(pos != std::string::npos){
+                    counter += stutterFrequency;
+                    if(counter >= 1){
+                        counter -= 1;
+                        char firstChar = (*i)[0];
+                        char secondChar = (*i)[1];
+                        std::string stringToImplement = firstChar + secondChar + "-" + tolower(secondChar);
+                        word.replace(pos, (*i).length(), stringToImplement);
+                    }
+                }
+            }
+        }
+    }
+
+    static void addUwUByWord(std::vector<std::string> &words, const float &uwuFrequency){
+        float counter = 0;
+        std::string foundText;
+        for(std::string &word : words){
+            std::string tempWord = trim(word);
+            if(tempWord.empty() || tempWord == "\n")
+                continue;
+            std::vector<size_t> positions;
+            size_t pos = 0;
+            while(true){
+                size_t previousPos = pos;
+                pos = word.find('\n', pos+1);
+                if(pos == std::string::npos)
+                    break;
+                if(pos != previousPos + 1)
+                    positions.push_back(pos);
+            }
+            size_t offset = 0;
+            for(long i = 0; i < positions.size(); i++){
+                counter += uwuFrequency;
+                if(counter >= 1){
+                    counter -= 1;
+                    word.replace(positions[i] + offset, 1, " UwU\n");
+                    offset += 4;
+                }
+            }
+        }
+    }
+
+    static void uwuifyByWord(std::string &text, const float &stutterFrequency, const float &uwuFrequency){
         const std::string space = " ";
         std::vector<std::string> words;
         size_t pos = text.find(space);
@@ -64,11 +124,13 @@ namespace UwUifier{
         text.clear();
         for(int index = 0; index < swapPairs.size(); index+=2)
             swapWithByWord(std::ref(words), swapPairs[index], swapPairs[index+1]);
+        //addStutterByWord(std::ref(words), stutterFrequency);
+        addUwUByWord(std::ref(words), uwuFrequency);
         for(std::string& word : words){
             text += word + space;
         }
         text.pop_back();
-        if(text[text.size()-1] == '\n')
+        while(text[text.size()-1] == '\n')
             text.pop_back();
     }
 
--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,12 @@
 #define NO_TEXT_ARG_PASSED "No text passed"
 #define NO_FILE_ARG_PASSED "No file passed"
 #define NO_OUTPUT_FILE_ARG_PASSED "No output file passed"
+#define NO_STUTTER_VALUE_PASSED "No stutter value passed"
+#define STUTTER_VALUE_OUTOFRANGE "Stutter value must be between 0 and 1"
+#define STUTTER_VALUE_NAN "Passed stutter value isn't a number"
+#define NO_UWU_FREQUENCY_PASSED "No uwu frequency passed"
+#define UWU_FREQUENCY_OUTOFRANGE "UwU frequency must be between 0 and 1"
+#define UWU_FREQUENCY_NAN "Passed uwu frequency isn't a number"
 #define FILE_DOESNT_EXIST "Input file doesn't exist"
 #define TEXT_OR_FILE_ARGS_NOT_PASSED "Neither \"-t\" or \"-f\" arguments passed"
 #define TEXT_AND_FILE_ARGS_PASSED "Both \"-t\" and \"-f\" arguments passed"
@@ -34,6 +40,12 @@
 #define NO_TEXT_ARG_PASSED "Nyo text passed"
 #define NO_FILE_ARG_PASSED "Nyo fiwe passed"
 #define NO_OUTPUT_FILE_ARG_PASSED "Nyo output fiwe passed"
+#define NO_STUTTER_VALUE_PASSED "Nyo stuttew vawue passed"
+#define STUTTER_VALUE_OUTOFRANGE "Stuttew vawue must be between 0 and 1"
+#define STUTTER_VALUE_NAN "Passed stuttew vawue isn't a numbew"
+#define NO_UWU_FREQUENCY_PASSED "Nyo uwu fwequency passed"
+#define UWU_FREQUENCY_OUTOFRANGE "UwU fwequency must be between 0 and 1"
+#define UWU_FREQUENCY_NAN "Passed uwu fwequency isn't a numbew"
 #define FILE_DOESNT_EXIST "Input fiwe doesn't exist"
 #define TEXT_OR_FILE_ARGS_NOT_PASSED "Neithew \"-t\" or \"-f\" awguments passed"
 #define TEXT_AND_FILE_ARGS_PASSED "Both \"-t\" and \"-f\" awguments passed"
@@ -58,8 +70,12 @@
 #define OUTPUT_FILE_CMD2 "--output"
 #define FORCE_FILE_WRITE1 "--force"
 #define FORCE_FILE_WRITE2 "--fowce"
-#define CMD_COUNT 12
-const char* ALL_CMDS[CMD_COUNT] = {HELP_CMD1, HELP_CMD2, HELP_CMD3, TEXT_CMD1, TEXT_CMD2, FILE_CMD1, FILE_CMD2, FILE_CMD3, OUTPUT_FILE_CMD1, OUTPUT_FILE_CMD2, FORCE_FILE_WRITE1, FORCE_FILE_WRITE2};
+#define STUTTER_FREQUENCE1 "-s"
+#define STUTTER_FREQUENCE2 "--stutter"
+#define UWU_FREQUENCY1 "-u"
+#define UWU_FREQUENCY2 "--uwu"
+#define CMD_COUNT 16
+const char* ALL_CMDS[CMD_COUNT] = {HELP_CMD1, HELP_CMD2, HELP_CMD3, TEXT_CMD1, TEXT_CMD2, FILE_CMD1, FILE_CMD2, FILE_CMD3, OUTPUT_FILE_CMD1, OUTPUT_FILE_CMD2, FORCE_FILE_WRITE1, FORCE_FILE_WRITE2, STUTTER_FREQUENCE1, STUTTER_FREQUENCE2, UWU_FREQUENCY1, UWU_FREQUENCY2};
 
 static std::string EXEC_FOLDER_PATH;
 static std::string EXECUTABLE_PATH;
@@ -92,6 +108,7 @@ int main(int argc, char **argv) {
         args.push_back(argv[index]);
     bool fromText = false, fromFile = false, writeToFile = false, forceWriteToFile = false;
     std::string text, inputFile, outputFile;
+    float stutterFrequency = .15f, uwuFrequency = .3;
     for(int index = 0; index < args.size(); index++){
         if(!checkIfTextIsCommand(args[index])){
             Logger::LogError(INVALID_COMMAND(args[index]));
@@ -144,6 +161,46 @@ int main(int argc, char **argv) {
         }
         if(args[index] == FORCE_FILE_WRITE1 || args[index] == FORCE_FILE_WRITE2)
             forceWriteToFile = true;
+        if(args[index] == STUTTER_FREQUENCE1 || args[index] == STUTTER_FREQUENCE1){
+            if(++index >= args.size()){
+                Logger::LogError(NO_STUTTER_VALUE_PASSED);
+                return 1;
+            }
+            if(checkIfTextIsCommand(args[index])){
+                Logger::LogError(NO_STUTTER_VALUE_PASSED);
+                return 1;
+            }
+            try{
+                stutterFrequency = std::stof(args[index]);
+                if(stutterFrequency < 0 || stutterFrequency > 1){
+                    Logger::LogError(STUTTER_VALUE_OUTOFRANGE);
+                    return 1;
+                }
+            }catch(...){
+                Logger::LogError(STUTTER_VALUE_NAN);
+                return 1;
+            }
+        }
+        if(args[index] == UWU_FREQUENCY1 || args[index] == UWU_FREQUENCY2){
+            if(++index >= args.size()){
+                Logger::LogError(NO_UWU_FREQUENCY_PASSED);
+                return 1;
+            }
+            if(checkIfTextIsCommand(args[index])){
+                Logger::LogError(NO_UWU_FREQUENCY_PASSED);
+                return 1;
+            }
+            try{
+                uwuFrequency = std::stof(args[index]);
+                if(uwuFrequency < 0 || uwuFrequency > 1){
+                    Logger::LogError(UWU_FREQUENCY_OUTOFRANGE);
+                    return 1;
+                }
+            }catch(...){
+                Logger::LogError(UWU_FREQUENCY_NAN);
+                return 1;
+            }
+        }
     }
     if(!fromText && !fromFile){
         Logger::LogError(TEXT_OR_FILE_ARGS_NOT_PASSED);
@@ -159,8 +216,6 @@ int main(int argc, char **argv) {
             return 1;
         }
     }
-    if(fromText)
-        UwUifier::uwuifyByWord(text);
     if(fromFile){
         std::stringstream stream;
         std::string line;
@@ -176,8 +231,8 @@ int main(int argc, char **argv) {
             stream.str(std::string());
         }
         file.close();
-        UwUifier::uwuifyByWord(text);
     }
+    UwUifier::uwuifyByWord(text, stutterFrequency, uwuFrequency);
     if(writeToFile){
         std::fstream file;
         file.open(outputFile, std::ios::out | std::ios::trunc);
