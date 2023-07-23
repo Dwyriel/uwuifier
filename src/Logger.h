1e2b566c0060ca555c4c3120027938435296b324
Dwyriel <dwyriel333@hotmail.com>
2023-07-23 20:03:09 -0300

    Starting from scratch

--- src/Logger.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "Logger.cpp"
-//tried to make this better by exporting only what's needed, but linker would never find the definition correctly.. I'm dumb and don't really understand this very well but I've wasted many hours already on this so whatever, it's going to be done in the worse way possible.. again
